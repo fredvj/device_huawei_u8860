c0ff7cb1e20786b995501b242f05c46e0d79ae95
fredvj <fredvjupiter@googlemail.com>
2012-07-21 22:58:55 +0200

    Typo in makefile that broke recovery

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),u8860)
-include $(call all-makesfile-under,$(LOCAL_PATH))
+include $(call all-makefiles-under,$(LOCAL_PATH))
 # include $(call all-named-subdir-makefiles, recovery)
 endif
