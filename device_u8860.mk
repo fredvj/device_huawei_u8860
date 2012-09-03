34b426c035c38b302236d64cf4dc2dffec60eb31
fredvj <fredvjupiter@googlemail.com>
2012-09-03 21:07:35 +0200

    Add a couple of init files and copy rmt_storage including libs and linker to prevent infamous two minute reboot

--- device_u8860.mk
+++ device_u8860.mk
@@ -38,11 +38,29 @@ PRODUCT_PACKAGES += \
 	libtilerenderer \
 	libQcomUI
 
-# Vold config
+# Vold config, boot logo & init scripts
 
 PRODUCT_COPY_FILES += \
 	device/huawei/u8860/vold.fstab:system/etc/vold.fstab \
-	device/huawei/u8860/init.huawei.rc:root/init.huawei.rc
+	device/huawei/u8860/boot-u8860.rle:root/initlogo.rle \
+	device/huawei/u8860/init.huawei.rc:root/init.huawei.rc \
+	device/huawei/u8860/init.target.rc:root/init.target.rc \
+	device/huawei/u8860/ueventd.huawei.rc:root/ueventd.huawei.rc
+
+# Recovery magic
+
+PRODUCT_COPY_FILES += \
+	device/huawei/u8860/recovery/linker:root/sbin/linker \
+	device/huawei/u8860/recovery/rmt_storage:root/sbin/rmt_storage \
+	device/huawei/u8860/recovery/strace:root/sbin/strace \
+	device/huawei/u8860/recovery/libc.so:root/vendor/lib/libc.so \
+	device/huawei/u8860/recovery/libcutils.so:root/vendor/lib/libcutils.so \
+	device/huawei/u8860/recovery/libfotaagent.so:root/vendor/lib/libfotaagent.so \
+	device/huawei/u8860/recovery/liblog.so:root/vendor/lib/liblog.so \
+	device/huawei/u8860/recovery/libm.so:root/vendor/lib/libm.so \
+	device/huawei/u8860/recovery/libstdc++.so:root/vendor/lib/libstdc++.so \
+	device/huawei/u8860/recovery/libz.so:root/vendor/lib/libz.so
+
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_u8860
