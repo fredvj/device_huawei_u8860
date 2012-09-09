f830f2366e0840201158a78977568a38e74f43d0
fredvj <fredvjupiter@googlemail.com>
2012-09-09 02:24:22 +0200

    Add makefile to copy resource files when building the recovery image

---
+++ recovery/recovery_resources.mk
@@ -0,0 +1,54 @@
+# Recovery image resources for charge mode
+
+PRODUCT_COPY_FILES += \
+device/huawei/u8860/recovery/res/images/480x854/01.png:recovery/root/res/images/480x854/01.png \
+device/huawei/u8860/recovery/res/images/480x854/02.png:recovery/root/res/images/480x854/02.png \
+device/huawei/u8860/recovery/res/images/480x854/03.png:recovery/root/res/images/480x854/03.png \
+device/huawei/u8860/recovery/res/images/480x854/04.png:recovery/root/res/images/480x854/04.png \
+device/huawei/u8860/recovery/res/images/480x854/05.png:recovery/root/res/images/480x854/05.png \
+device/huawei/u8860/recovery/res/images/480x854/06.png:recovery/root/res/images/480x854/06.png \
+device/huawei/u8860/recovery/res/images/480x854/07.png:recovery/root/res/images/480x854/07.png \
+device/huawei/u8860/recovery/res/images/480x854/08.png:recovery/root/res/images/480x854/08.png \
+device/huawei/u8860/recovery/res/images/480x854/09.png:recovery/root/res/images/480x854/09.png \
+device/huawei/u8860/recovery/res/images/480x854/10.png:recovery/root/res/images/480x854/10.png \
+device/huawei/u8860/recovery/res/images/480x854/11.png:recovery/root/res/images/480x854/11.png \
+device/huawei/u8860/recovery/res/images/480x854/12.png:recovery/root/res/images/480x854/12.png \
+device/huawei/u8860/recovery/res/images/480x854/13.png:recovery/root/res/images/480x854/13.png \
+device/huawei/u8860/recovery/res/images/480x854/14.png:recovery/root/res/images/480x854/14.png \
+device/huawei/u8860/recovery/res/images/480x854/15.png:recovery/root/res/images/480x854/15.png \
+device/huawei/u8860/recovery/res/images/480x854/16.png:recovery/root/res/images/480x854/16.png \
+device/huawei/u8860/recovery/res/images/480x854/17.png:recovery/root/res/images/480x854/17.png \
+device/huawei/u8860/recovery/res/images/480x854/18.png:recovery/root/res/images/480x854/18.png \
+device/huawei/u8860/recovery/res/images/480x854/19.png:recovery/root/res/images/480x854/19.png \
+device/huawei/u8860/recovery/res/images/480x854/batt00.png:recovery/root/res/images/480x854/batt00.png \
+device/huawei/u8860/recovery/res/images/480x854/batt01.png:recovery/root/res/images/480x854/batt01.png \
+device/huawei/u8860/recovery/res/images/480x854/batt02.png:recovery/root/res/images/480x854/batt02.png \
+device/huawei/u8860/recovery/res/images/480x854/batt03.png:recovery/root/res/images/480x854/batt03.png \
+device/huawei/u8860/recovery/res/images/480x854/batt04.png:recovery/root/res/images/480x854/batt04.png \
+device/huawei/u8860/recovery/res/images/480x854/batt05.png:recovery/root/res/images/480x854/batt05.png \
+device/huawei/u8860/recovery/res/images/480x854/batt06.png:recovery/root/res/images/480x854/batt06.png \
+device/huawei/u8860/recovery/res/images/480x854/batt07.png:recovery/root/res/images/480x854/batt07.png \
+device/huawei/u8860/recovery/res/images/480x854/batt08.png:recovery/root/res/images/480x854/batt08.png \
+device/huawei/u8860/recovery/res/images/480x854/batt09.png:recovery/root/res/images/480x854/batt09.png \
+device/huawei/u8860/recovery/res/images/480x854/batt10.png:recovery/root/res/images/480x854/batt10.png \
+device/huawei/u8860/recovery/res/images/480x854/bg.png:recovery/root/res/images/480x854/bg-png \
+device/huawei/u8860/recovery/res/images/480x854/level00.png:recovery/root/res/images/480x854/level00.png \
+device/huawei/u8860/recovery/res/images/480x854/level100.png:recovery/root/res/images/480x854/level100.png \
+device/huawei/u8860/recovery/res/images/480x854/level10.png:recovery/root/res/images/480x854/level10.png \
+device/huawei/u8860/recovery/res/images/480x854/level20.png:recovery/root/res/images/480x854/level20.png \
+device/huawei/u8860/recovery/res/images/480x854/level30.png:recovery/root/res/images/480x854/level30.png \
+device/huawei/u8860/recovery/res/images/480x854/level40.png:recovery/root/res/images/480x854/level40.png \
+device/huawei/u8860/recovery/res/images/480x854/level50.png:recovery/root/res/images/480x854/level50.png \
+device/huawei/u8860/recovery/res/images/480x854/level60.png:recovery/root/res/images/480x854/level60.png \
+device/huawei/u8860/recovery/res/images/480x854/level70.png:recovery/root/res/images/480x854/level70.png \
+device/huawei/u8860/recovery/res/images/480x854/level80.png:recovery/root/res/images/480x854/level80.png \
+device/huawei/u8860/recovery/res/images/480x854/level90.png:recovery/root/res/images/480x854/level90.png \
+device/huawei/u8860/recovery/res/images/480x854/level_wait.png:recovery/root/res/images/480x854/level_wait.png \
+ \
+device/huawei/u8860/recovery/res/images/normal/level0.png:recovery/root/res/images/normal/level0.png \
+device/huawei/u8860/recovery/res/images/normal/level1.png:recovery/root/res/images/normal/level1.png \
+device/huawei/u8860/recovery/res/images/normal/level2.png:recovery/root/res/images/normal/level2.png \
+device/huawei/u8860/recovery/res/images/normal/level3.png:recovery/root/res/images/normal/level3.png \
+device/huawei/u8860/recovery/res/images/normal/level4.png:recovery/root/res/images/normal/level4.png \
+device/huawei/u8860/recovery/res/images/normal/level5.png:recovery/root/res/images/normal/level5.png
+
